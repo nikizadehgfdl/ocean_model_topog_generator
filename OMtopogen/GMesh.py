Try to fix the roughness calculation
- The existing plane fit formulation of roughness is wrong.
- This update brings a simple implementation of roughness
  to be just the variance of heights for source data points "contained" in a model grid cell.

OMtopogen/GMesh.py
@@ -219,15 +219,15 @@ class GMesh:
         coarser_mesh.h_max[:-1,:-1] = np.maximum(coarser_mesh.h_max[:-1,:-1],self.height[1::2,0:-1:2])
         coarser_mesh.h_max[:-1,:-1] = np.maximum(coarser_mesh.h_max[:-1,:-1],self.height[0:-1:2,1::2])
 
-        coarser_mesh.h_std = np.zeros(coarser_mesh.height.shape)
-        coarser_mesh.zm = coarser_mesh.height
-        coarser_mesh.xm = fourPointAve(self.xm)
-        coarser_mesh.ym = fourPointAve(self.ym)
-        coarser_mesh.xxm = fourPointAve(self.xxm)
-        coarser_mesh.yym = fourPointAve(self.yym)
-        coarser_mesh.xym = fourPointAve(self.xym)
-        coarser_mesh.xzm = fourPointAve(self.xzm)
-        coarser_mesh.yzm = fourPointAve(self.yzm)
+        #coarser_mesh.zm = coarser_mesh.height
+        #coarser_mesh.xm = fourPointAve(self.xm)
+        #coarser_mesh.ym = fourPointAve(self.ym)
+        #coarser_mesh.xxm = fourPointAve(self.xxm)
+        #coarser_mesh.yym = fourPointAve(self.yym)
+        #coarser_mesh.zzm = fourPointAve(self.zzm)
+        #coarser_mesh.xym = fourPointAve(self.xym)
+        #coarser_mesh.xzm = fourPointAve(self.xzm)
+        #coarser_mesh.yzm = fourPointAve(self.yzm)
 
     def mdist(x1,x2):
         """Returns positive distance modulo 360."""
@@ -315,142 +315,27 @@ class GMesh:
         i,j = self.find_nn_uniform_source(xs,ys)
         self.height = np.zeros(self.lon.shape)
         self.height[:,:] = zs[j[:],i[:]]
-        self.h_std = np.zeros(self.lon.shape)
         self.h_min = np.zeros(self.lon.shape)
         self.h_max = np.zeros(self.lon.shape)
 	# Quantities needed for calculating the roughness
-        self.xm = np.zeros(self.lon.shape)
-        self.ym = np.zeros(self.lon.shape)
-        self.zm = np.zeros(self.lon.shape)
-        self.xxm = np.zeros(self.lon.shape)
-        self.yym = np.zeros(self.lon.shape)
-        self.xym = np.zeros(self.lon.shape)
-        self.xzm = np.zeros(self.lon.shape)
-        self.yzm = np.zeros(self.lon.shape)
-        self.xm[:,:] = xs[i[:]]
-        self.ym[:,:] = ys[j[:]]
-        self.zm[:,:] = zs[j[:],i[:]]
-        self.xxm[:,:] = self.xm[:,:] * self.xm[:,:]
-        self.yym[:,:] = self.ym[:,:] * self.ym[:,:]
-        self.xym[:,:] = self.xm[:,:] * self.ym[:,:]
-        self.xzm[:,:] = self.xm[:,:] * self.zm[:,:]
-        self.yzm[:,:] = self.ym[:,:] * self.zm[:,:]
+        #self.xm = np.zeros(self.lon.shape)
+        #self.ym = np.zeros(self.lon.shape)
+        #self.zm = np.zeros(self.lon.shape)
+        #self.xxm = np.zeros(self.lon.shape)
+        #self.yym = np.zeros(self.lon.shape)
+        #self.zzm = np.zeros(self.lon.shape)
+        #self.xym = np.zeros(self.lon.shape)
+        #self.xzm = np.zeros(self.lon.shape)
+        #self.yzm = np.zeros(self.lon.shape)
+        #self.xm[:,:] = xs[i[:]]
+        #self.ym[:,:] = ys[j[:]]
+        #self.zm[:,:] = zs[j[:],i[:]]
+        #self.xxm[:,:] = self.xm[:,:] * self.xm[:,:]
+        #self.yym[:,:] = self.ym[:,:] * self.ym[:,:]
+        #self.zzm[:,:] = self.zm[:,:] * self.zm[:,:]
+        #self.xym[:,:] = self.xm[:,:] * self.ym[:,:]
+        #self.xzm[:,:] = self.xm[:,:] * self.zm[:,:]
+        #self.yzm[:,:] = self.ym[:,:] * self.zm[:,:]
 
         return
-
-    def least_square_plane_estimate(self, xs,ys,zs):
-        """This function returns the estimates for h2 and h and also mean,min,max of the date
-           in each grid cell. """
-        """It estimates h and h2 passing a least-square plane through the data points in each grid cell."""
-        """The plane is calculated via a standard algorithm explained in text books (T. Shifrin, Multivariable Mathematic, p227)"""
-        """h2 is estiamted as the standard deviation of z-distance of data points in the grid cell from the fit plane."""
-        """h  is estiamted as the value of plane for z calculated at the corner of the grid cell."""
-
-        epsilon=1.0e-5
-        #indices of nearest neighbor source point to each target mesh point 
-        ti,tj = self.find_nn_uniform_source(xs,ys)
-        
-        #Initialize with nans as missing values
-        #This will leave some mesh point values as nans
-        Zmean=np.empty(self.lon.shape)*np.nan
-        Zmin =np.empty(self.lon.shape)*np.nan
-        Zmax =np.empty(self.lon.shape)*np.nan
-        Zstd =np.empty(self.lon.shape)*np.nan
-
-        #Loop over each grid cell. Is there a numpy way of doing this?
-        dlon=np.roll(self.lon,shift=-1,axis=1)-self.lon
-        dlat=np.roll(self.lat,shift=-1,axis=0)-self.lat
-        dti =np.roll(ti,shift=-1,axis=1)-ti
-        dtj =np.roll(tj,shift=-1,axis=0)-tj
-        #fix the last elements #halo?
-        dlon[:,-1]=dlon[:,-2] 
-        dlat[-1,:]=dlat[-2,:] 
-        dti[:,-1]=dti[:,-2] 
-        #print(dtj)
-        dtj[-1,:]=dtj[-2,:] 
-        #print(dtj)
-        for J in range(0,self.lat.shape[0]):
-            for I in range(0,self.lon.shape[1]): 
-                #Initialize to the NN source value. Reasonable?
-                znn=zs[tj[J,I],ti[J,I]]
-                Zmean[J,I]= znn
-                Zmin[J,I] = znn
-                Zmax[J,I] = znn
-                Zstd[J,I] = 0.0
-                
-                #bounds of a target cell
-                lon_min=self.lon[J,I]
-                lon_max=lon_min+dlon[J,I]
-                lat_min=self.lat[J,I]
-                lat_max=lat_min+dlat[J,I]
-                #bounds of indexes of NN source cell
-                tj_min=tj[J,I]
-                tj_max=tj_min+dtj[J,I]
-                ti_min=ti[J,I]
-                ti_max=ti_min+dti[J,I]
-                ti_max=min(ti_max,xs.shape[0]-1)
-                tj_max=min(tj_max,ys.shape[0]-1)
-                #We don't know how many data points are in each grid cell. Let's say it's M.
-                #According to the algorithm, we need to create 
-                X=[]
-                Y=[]
-                Z=[]
-                #print('J,I ',J,I)
-                #print(lon_min,lon_max)
-                #print(ti_min,ti_max)
-                #print(lat_min,lat_max)
-                #print(tj_min,tj_max,dtj[J,I])
-                for jj in range(tj_min,tj_max+1):
-                    #print(ys[jj],lat_min,lat_max)
-                    if((ys[jj]>=lat_min) and (ys[jj]<lat_max)):
-                        for ii in range(ti_min,ti_max+1):
-                            if(xs[ii] >= lon_min and xs[ii] < lon_max):
-                                #print('jj,ii',jj,ii)
-                                X.append(np.array(xs[ii]))
-                                Y.append(np.array(ys[jj]))
-                                Z.append(np.array(zs[jj,ii]))
-                #print(len(Z))
-                if(len(Z)==0):
-                    continue
-                #cast these in numpy arrays
-                X = np.asarray(X) 
-                Y = np.asarray(Y)
-                Z = np.asarray(Z)
-                #The algorithm fits a plane z=P(x,y) by minimizing \sum_i (z_i - P(x_i,y_i)) 
-                #It shows that
-                #1. P is of the form P = zm + ax*(x-xm) + ay*(y-ym), 
-                #                    xm,ym,zm being the means of data x_i,y_i,z_i respectively
-                #     I.e., the least square plane passes through the point (xm,ym,zm)
-                #2. It gives the following formula for ax and ay (solution of 2by2 linear system)
-                
-                N=X.size
-                xm=np.sum(X)/N
-                ym=np.sum(Y)/N
-                zm=np.sum(Z)/N
-                sxx=np.dot(X-xm,X-xm)#/N
-                syy=np.dot(Y-ym,Y-ym)#/N
-                sxy=np.dot(X-xm,Y-ym)#/N
-                syz=np.dot(Y-ym,Z-zm)#/N
-                sxz=np.dot(X-xm,Z-zm)#/N
-                
-                det=(sxx*syy-sxy*sxy)
-                if(abs(det) < epsilon): #No solutions
-                    continue
-                ax=(sxz*syy-syz*sxy)/det
-                ay=(syz*sxx-sxz*sxy)/det
-                d=Z-zm - ax*(X-xm) - ay*(Y-ym)
-                
-                Zstd[J,I]=d.std()
-                #Zij[J,I] = zm + ax*(self.lon[J,I]-xm)+ay*(self.lat[J,I]-ym) #corner fit value
-                Zmean[J,I]= zm
-                Zmin[J,I] = Z.min()
-                Zmax[J,I] = Z.max()
-
-                #Check: The sum of Distances must be very small, almost zero
-                #assert abs(d.sum())<0.00001, "Bad fit: The sum of Distances is large at "+str(I)+","+str(J)+" = "+str(d.sum())+" compared to min  "+str(Zmin[J,I])
-                if(abs(d.sum())>epsilon):
-                    print("Bad fit: The sum of Distances is large at ("+str(I)+","+str(J)+") = "+str(d.sum())+" compared to min  "+str(Zmin[J,I]))
-
-        return Zstd,Zmean,Zmin,Zmax
-
 

OMtopogen/create_topog_refinedSampling.py
@@ -209,7 +209,63 @@ def do_RSC(lon,lat,topo_lons,topo_lats,topo_elvs, max_mb=8000000, max_refine=32,
     for i in reversed(range(1,len(Glist))):   # 1, makes it stop at element 1 rather than 0
         Glist[i].coarsenby2(Glist[i-1])
 
-    print("Roughness calculation via plane fit")
+    print("Roughness calculation, simple ")
+    D_std = do_roughness_simple(Glist)
+    return Glist[0].height,D_std,Glist[0].h_min,Glist[0].h_max, hits
+    
+
+def do_roughness_simple(Glist):
+    #Simple Roughness calculation: 
+    #   roughness=standard deviation of heights of source points contained/hit in the coarse grid cell
+    #
+    G=Glist[0]
+    #The refined mesh has a shape of (2*nj-1,2*ni-1) rather than (2*nj,2*ni) and hence
+    #is missing the last row/column by construction!
+    #So, the finest mesh does not have (rf*nj,rf*ni) points but is smaller by ...
+    #Bring it to the same shape as (rf*nj,rf*ni) by padding with zeros.
+    #This is for algorithm convenience and we remove the contribution of them later.
+    rf=2**(len(Glist)-1) #refinement factor
+    zs=extend_by_zeros(Glist[-1].height,(G.height.shape[0]*rf,G.height.shape[1]*rf))
+    #Fold the array so that we can use .std() method
+    #This is why we wanted to have a (nj*rf,ni*rf) shape arrays and padded with zeros above.
+    zs_coarse=np.reshape(zs,(G.height.shape[0],rf,G.height.shape[1],rf))
+    #Calculate topography roughness as the standard deviation of zs on each coarse (model) grid cell
+    D_std = zs_coarse.std(axis=(1,3))
+
+    return D_std
+
+
+def do_roughness_planefit(Glist):   
+    #Roughness calculation by plane fitting
+    #Note that the least-square plane passes through the mean data point.
+    #
+    #Plane equation: z-zm = alph*(x-xm)+beta*(y-ym)
+    #where xm,ym,zm are means of (x,y,z) cartesian coordinates of source points that are "hit" in the (coarse) model grid cell
+    #
+    #alph and beta are obtained by minimizing the sum of square of z-distance of the data points from the plane
+    # S = \sum_{p} (z_p - z_plane(x_p,y_p))^2 ; where p stands for the data points hit inside the model grid cell
+    #   = \sum_{p} (z_p-zm - alph*(x_p-xm)- beta*(y_p-ym) )^2
+    #
+    #By setting derivatives of S w.r.t. alph and beta to zero we can find the extremums of S.
+    #It can be shown that alph and beta are related to averages and covariances of the source point coordinates
+    #as follows
+    #Calculate the slopes of the planes on the coarsest (model) grid cells
+    G=Glist[0] #coarsest grid (model grid)
+    epsilon=1.0e-20 #To avoid negative underflow
+    denom=(G.xxm-G.xm*G.xm)*(G.yym-G.ym*G.ym)-(G.xym-G.xm*G.ym)*(G.xym-G.xm*G.ym)
+    alph=(G.xzm-G.xm*G.zm)*(G.yym-G.ym*G.ym)-(G.yzm-G.ym*G.zm)*(G.xym-G.xm*G.ym)/(denom+epsilon)
+    beta=(G.yzm-G.ym*G.zm)*(G.xxm-G.xm*G.xm)-(G.xzm-G.xm*G.zm)*(G.xym-G.xm*G.ym)/(denom+epsilon)
+    #In the above expression (xm,ym,zm) are the means of (x,y,z) coordinates of the source points in the coarse grid cell
+    D_std=np.zeros(G.zm.shape)
+    D_std= (G.zzm-G.zm*G.zm) + alph*alph*(G.xxm-G.xm*G.xm) + beta*beta*(G.yym-G.ym*G.ym)
+    -2*alph*(G.xzm-G.xm*G.zm) -2*beta*(G.yzm-G.ym*G.zm) +2*alph*beta*(G.xym-G.xm*G.ym)
+    
+    rf=2**(len(Glist)-1) #refinement factor
+    #The number of source points hit in each coarse grid cell is 
+    D_std=D_std/rf/rf
+    return D_std
+
+def do_roughness_old_wrong(Glist):   
     #Roughness calculation by plane fitting
     #Calculate the slopes of the planes on the coarsest (model) grid cells
     G=Glist[0]
@@ -251,12 +307,7 @@ def do_RSC(lon,lat,topo_lons,topo_lats,topo_elvs, max_mb=8000000, max_refine=32,
     epsilon=1.0e-20 #To avoid negative underflow
     D_std[:,:] = D_times_denom_coarse_std[:,:]/(denom[:,:]+epsilon)
 
-    print("")
-    #print("Writing ...")
-    #filename = 'topog_refsamp_BP.nc'+str(b)
-    #write_topog(Glist[0].height,fnam=filename,no_changing_meta=True)
-    #print("haigts shape:", lons[b].shape,Hlist[b].shape)
-    return Glist[0].height,D_std,Glist[0].h_min,Glist[0].h_max, hits
+    return D_std
 
 def do_block(blk,lons,lats,topo_lons,topo_lats,topo_elvs,max_refine):
     print("Doing block ",blk)